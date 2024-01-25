fd50623284371ad8743b76e55a978edd73be6bfa
Roneisha Still <roneishastill@Roneishas-iMac.lan>
2024-01-24 23:02:19 -0500

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Script");
+
+var userName="Roneisha";// creating and assigning value to the var
+
+userName = "James"; // changing the var value
+
+console.log(userName);
+
+var emplyeeName="Kory";
+var employeeAge = 27;
+var employeeCountry = "USA";
+var employeePhone="55589076";
+var employeeSalaryMonthly=20000;
+var employeeSalaryYearly= employeeSalaryMonthly *12;
+
+document.write(
+    `
+    <h3>PrimeVideo</h3>
+    <p>Name: ${employeeName} </p>
+    <p>Age: ${employeeAge} </p>
+    `
+);
